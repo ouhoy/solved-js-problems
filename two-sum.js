3d88236aef7f56505998b364e5d2b847a78f8a97
ouhoy <kimkinon@gmail.com>
2022-01-09 20:37:35 +0100

    Create two sum js
    The problem is unsolved yet

---
+++ two-sum.js
@@ -0,0 +1,30 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+
+const twoSum = function(nums, target) {
+    
+    for (let i =0; i< nums.length; i++) { //o(n)
+        if(nums[i] + nums[i + 1] === target) {
+            return [i , i+1]
+        }
+    }
+    
+    
+};
+
+//This problem is from leetCode https://leetcode.com/problems/two-sum/description/
